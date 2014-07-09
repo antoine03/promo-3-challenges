43e9ff1266bccd598a29b31d523398956cec43bb
Antoine Akel <akelantoine@gmail.com>
2014-07-09 09:11:21 +0200

    exercie 5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,11 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts " what do you want to tell to your coach ?"
+answer = gets.chomp
 
+until answer == "i'm going to work right now SIR !"
+
+puts coach_answer(answer)
+puts "posez une question à votre coach"
+answer = get.chomp
+end
