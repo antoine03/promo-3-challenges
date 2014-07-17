9e603f831e86d56d953a974ad57fb04df3ab28ac
Antoine Akel <akelantoine@gmail.com>
2014-07-17 16:03:21 +0200

    pas fini

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -1,7 +1,20 @@
 def old_roman_numeral(an_integer)
   #TODO: translate integer in roman number - old-style way
+  roman_numbers = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I"=>1}
+  roman_string = ""
+  roman_numbers.each do |key, value|
+    quotient = an_integer / value
+    an_integer = an_integer % value
+    if quotient >= 1
+      roman_string += key*quotient
+    end
+  end
+  roman_string
 end
 
-def roman_numeral(an_integer)
+def new_roman_numeral(an_integer)
   #TODO: translate integer in roman number - modern-style way
+
 end
+
+puts new_roman_numeral(19)
