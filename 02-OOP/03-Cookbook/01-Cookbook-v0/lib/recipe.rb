ad9c932bb6ef8a93bbd3c8257fe9c22f2920ea64
Antoine Akel <akelantoine@gmail.com>
2014-07-21 11:29:24 +0200

    solved

---
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/recipe.rb
@@ -0,0 +1,14 @@
+class Recipe
+
+  attr_reader :name, :description
+
+  attr_writer :name, :description
+
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+
+end
+
