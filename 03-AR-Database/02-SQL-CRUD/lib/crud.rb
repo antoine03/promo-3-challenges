9c1e8f2511b85dca02cd0d863cb3185ec537760f
Antoine Akel <akelantoine@gmail.com>
2014-07-28 11:15:25 +0200

    done

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -1,32 +1,23 @@
-require 'sqlite3'
-
-def create_scheme(db)
-  #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
-  db.execute(sql)
-end
-
 def create_post(db, post)
-  #TODO: add a new post to your db
+  db.execute("INSERT INTO posts (name, source_url, date, rating) VALUES ('#{post[:name]}', '#{post[:source_url]}', '#{post[:date]}', '#{post[:rating]}')")
 end
 
 def get_posts(db)
-   #TODO: list all posts
+  db.execute("SELECT * FROM posts")
 end
 
 def get_post(db, id)
-  #TODO: get a specific post
+  db.execute("SELECT * FROM posts where id = #{id}")
 end
 
 def update_post(db, id, name)
-	#TODO: update a post's name in your db
+  db.execute("UPDATE posts SET name = '#{name}' WHERE id = #{id}")
 end
 
 def delete_posts(db)
-  #TODO: delete a post in your db
+  db.execute("DELETE FROM posts")
 end
 
 def delete_post(db, id)
-  #TODO: delete a specific post in your db
+  db.execute("DELETE FROM posts WHERE Id = #{id}")
 end
--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -8,32 +8,46 @@ end
 db_path = "db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+#create_scheme(db)
 
 while true
 
   puts "Hey you, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
-  puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "3. Read a post"
+  puts "4. Delete all posts"
+  puts "5. Delete a post"
+  puts "6. Update a post"
+  puts "7. Exit"
+
   # TODO: add other CRUD tasks if you wish!
-	choice =  gets.chomp.to_i
-	
-	case choice
+  choice =  gets.chomp.to_i
+  p "-----------------------"
+  case choice
   when 1
     name = ask_and_get("name")
     source_url = ask_and_get("source url")
-    rating = ask_and_get("rating")
+    rating = ask_and_get("rating").to_i
     post = { name: name, source_url: source_url, date: Time.now, rating: rating }
     create_post(db, post)
   when 2
     get_posts(db)
   when 3
+    id = ask_and_get("id").to_i
+    get_post(db, id)
+  when 4
     delete_posts(db)
-  when 4 
+  when 5
+    id = ask_and_get("id").to_i
+    delete_post(db, id)
+  when 6
+    id = ask_and_get("id").to_i
+    name = ask_and_get("name")
+    update_post(db, id, name)
+  when 7
     break
-	end 
-	
+  end
+  p "-----------------------"
 end
+
