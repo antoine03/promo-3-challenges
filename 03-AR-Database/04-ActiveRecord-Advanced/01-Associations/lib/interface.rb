d0c2fd9594b377057ef8543bdc72525b5e9f4fb8
Antoine Akel <akelantoine@gmail.com>
2014-07-31 11:54:21 +0200

    Adding Migrations code

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
@@ -2,6 +2,10 @@ require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
 
+User.where(name: 'john Doe', email: 'john@me.com').first_or_create
+
+p User.last
+
 def ask(prompt)
   print "#{prompt} "
   gets.to_s.chomp
@@ -14,31 +18,40 @@ def create_post(user)
   post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
 
   # TODO: use ActiveRecord to add a new post for the user logged in!
+  user.posts.create(post_params)
 end
 
 def list_posts(user)
-  # TODO: use ActiveRecord to get all posts of the current user
+  #TODO: use ActiveRecord to get all posts of the current user
+  user.posts.each do |post|
+    puts '%-3s %-15s %-20s %-30s %s' % [post.id.to_s, post.name, post.source_url, post.date, post.rating.to_s]
+  end
 end
 
 def delete_posts(user)
   # TODO: use ActiveRecord to delete all posts of current user
+  user.posts.destroy_all
+end
+
+logged_in = false
+
+until logged_in
+  puts 'Please login with your <id>'
+  id = gets.chomp.to_i
+
+  user = User.find(id)
+  logged_in = true
+  # TODO: instantiate a user with his <id>
 end
 
 while true
-  logged_in = false
-
-  until logged_in
-    puts 'Please login with your <id>'
-    # TODO: instantiate a user with his <id>
-  end
-
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
 
-  case ask('>')
+  case ask('>').to_i
   when 1 then create_post(user)
   when 2 then list_posts(user)
   when 3 then delete_posts(user)
